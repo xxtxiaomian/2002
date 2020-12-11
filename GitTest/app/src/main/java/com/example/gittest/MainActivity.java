b505a04f95bae2c73b4d8b581a42a57dc921ebe1
xxtxiaomian <2358922725@qq.com>
2020-12-11 14:39:45 +0800

    git 使用

--- GitTest/app/src/main/java/com/example/gittest/MainActivity.java
+++ GitTest/app/src/main/java/com/example/gittest/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+        initData();
+    }
+
+    private void initData() {
+
     }
 
     private void initView() {
